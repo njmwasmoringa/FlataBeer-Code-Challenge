0cf3345ac27e037cdc4070639215c2e6fa3a82e9
Julius Mwangi <julius.mwangi@moringaschool.com>
2022-08-25 19:34:04 +0300

    Simplified code

--- src/index.js
+++ src/index.js
@@ -1,132 +1,38 @@
-// Code here
-const api_endpoint = 'http://localhost:3000';
-let selectedBeer;
-let beers;
+const apiHost = "http://localhost:3000";
 
-function getBeer(id = 1) {
-    return fetch(`${api_endpoint}/beers/${id}`).then(resp => resp.json());
+function getAndLoadAllBeers(){
+    fetch(`${apiHost}/beers`).then(resp=>resp.json()).then(beers=>{
+        document.getElementById('beer-list').innerHTML = beers
+        .map(beer=>`<li onClick="getAndLoadBeerDetails(${beer.id})">${beer.name}</li>`)
+        .join('');
+    })
 }
 
-function getAllBeers() {
-    return fetch(`${api_endpoint}/beers`).then(resp => resp.json());
+function getAndLoadBeerDetails(beerId){
+    fetch(`${apiHost}/beers/${beerId}`).then(resp=>resp.json()).then(beer=>{
+        console.log(beer);
+        document.getElementById('beer-name').innerHTML = beer.name;
+        document.getElementById('beer-image').src = beer.image_url;
+        document.getElementById('beer-description').innerHTML = beer.description;
+        document.getElementById('review-list').innerHTML = beer.reviews.map(review=>`<li>${review}</li>`).join('');
+    });
 }
 
-window.addEventListener('DOMContentLoaded', evt => {
-    const mainElement = document.getElementsByTagName('main')[0];
-    const beerListELement = document.querySelector('#beer-list');
-    const beerDetailsElement = document.querySelector('.beer-details');
-    const beerNameElement = document.querySelector('#beer-name');
-    const beerImageElement = document.querySelector('#beer-image');
-    const beerDescriptionElement = document.querySelector('#beer-description');
-    const beerDescriptionForm = document.querySelector('#description-form');
-    const beerReviewForm = document.querySelector('#review-form');
-    const reviewListElement = document.querySelector('#review-list');
-    const loader = document.createElement('div');
-    loader.setAttribute('id', 'loader');
-    loader.innerText = "loading..."
+document.addEventListener('DOMContentLoaded', ()=>{
+    getAndLoadAllBeers();
+    getAndLoadBeerDetails(1);
 
-    const toggleProgress = () => {
-        if (document.querySelector('#loader')) {
-            document.querySelector('#loader').remove();
-            beerDetailsElement.style.display = 'block';
-        }
-        else {
-            mainElement.insertBefore(loader, beerDetailsElement);
-            beerDetailsElement.style.display = 'none';
-        }
-    };
-
-    const updateBeersList = () => {
-        beerListELement.innerHTML = beers.map(beer => {
-            return `<li>
-                <a href="/index.html?id=${beer.id}" data-id="${beer.id}">${beer.name}</a>
-            </li>`;
-        }).join('');
-
-        beerListELement.querySelectorAll('a').forEach(link => {
-            link.addEventListener('click', (e) => {
-                e.preventDefault();
-                const beerId = e.target.dataset.id;
-                selectedBeer = beers.find(beer => beer.id == beerId);
-                updateBeerDetails(selectedBeer);
-            })
-        })
-    };
-
-    const updateBeerDetails = (beer) => {
-        beerImageElement.setAttribute('src', beer.image_url);
-        const spanLoader = document.createElement('div');
-        spanLoader.id = "imageLoader";
-        spanLoader.style.textAlign = "center";
-        spanLoader.innerHTML = 'Loaing Iamage...';
-        beerImageElement.parentNode.insertBefore(spanLoader, beerImageElement);
-        beerDescriptionElement.innerHTML = beer.description;
-        beerNameElement.innerHTML = beer.name;
-        reviewListElement.innerHTML = beer.reviews.map(review => {
-            return `<li>${review}</li>`;
-        }).join('');
-        
-        beerDescriptionForm.addEventListener('submit', submitDescription);
-        beerReviewForm.addEventListener('submit', submitReview);
-    }
-
-    /* Extract the query parameters from the URL */
-    const queryParamsString = location.search;
-    const queryParams = queryParamsString.substring(1, queryParamsString.length).split('&')
-        .reduce((a, b) => { const [k, v] = b.split('='); a[k] = v; return a }, {});
-
-    const beerId = queryParams.id || 1;
-
-
-    /* initialize content */
-    toggleProgress();
-    getBeer(beerId).then(beer => {
-        toggleProgress();
-        selectedBeer = beer;
-        updateBeerDetails(beer);
-    });
-
-    beerListELement.innerHTML = '';
-    getAllBeers().then((response)=>{ beers=response;  updateBeersList();});
-
-    beerImageElement.addEventListener('load', (evt) => {
-        if (document.getElementById('imageLoader')) {
-            document.getElementById('imageLoader').remove();
-        }
-    });
-
-    function submitDescription(evt){
+    document.getElementById('description-form').addEventListener('submit', (evt)=>{
         evt.preventDefault();
         const form = evt.target;
-        selectedBeer.description = form.querySelector('#description').value;
-        const index = beers.findIndex(b=>b.id == selectedBeer.id);
-        beers[index] = selectedBeer;
-        form.querySelector('#description').value = '';
-        beerDescriptionElement.innerHTML = selectedBeer.description;
-        fetch(`${encodeURI}/beers/${selectedBeer.id}`, {
-            method:'PUT', 
-            body:JSON.stringify(selectedBeer),
-            headers:{
-                'Content-Type':'application/json'
-            }
-        })
-    }
-    
-    function submitReview(evt){
+        document.getElementById('beer-description').innerHTML = form.description.value;
+        form.reset();
+    });
+
+    document.getElementById('review-form').addEventListener('submit', evt=>{
         evt.preventDefault();
         const form = evt.target;
-        selectedBeer.reviews.push(form.querySelector('#review').value);
-        form.querySelector('#review').value = '';
-        reviewListElement.innerHTML = selectedBeer.reviews.map(review => {
-            return `<li>${review}</li>`;
-        }).join('');
-        fetch(`${encodeURI}/beers/${selectedBeer.id}`, {
-            method:'PUT', 
-            body:JSON.stringify(selectedBeer),
-            headers:{
-                'Content-Type':'application/json'
-            }
-        })
-    }
-
-});
+        document.getElementById('review-list').innerHTML += `<li>${form.review.value}</li>`;
+        form.reset();
+    })
+})
